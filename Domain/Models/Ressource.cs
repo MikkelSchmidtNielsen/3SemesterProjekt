648ef87f15e7ac97079e244a1ac3dd5281942224
Sarah <peasblinky@gmail.com>
2025-11-13 10:51:15 +0100

    Added a new model (Ressource.cs) and a new xUnit Test (RessourceTests.cs)

---
+++ Domain/Models/Ressource.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Ressource
+    {
+        public int ResourceID { get; init; }
+        public string ResourceName { get; set; }
+        public string ResourceType { get; set; }
+        public double ResourceBasePrice { get; set; }
+
+        public Ressource(int resourceId, string resourceName, string resourceType, double resourceBasePrice)
+        {
+            ResourceID = resourceId;
+            ResourceName = resourceName;
+            ResourceType = resourceType;
+            ResourceBasePrice = resourceBasePrice;
+
+            ValidateInformation();
+        }
+
+        public bool ValidateInformation()
+        {
+            if (ResourceID == 0 || ResourceID == null)
+            {
+                throw new Exception("Resource ID is either 0 or null.");
+            }
+            else if (ResourceName == null)
+            {
+                throw new Exception("Resource name is null.");
+            }
+            else if(ResourceType == null)
+            {
+                throw new Exception("Resource type is null.");
+            }
+            else if(ResourceBasePrice == null)
+            {
+                throw new Exception("Resource price is null.");
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ UnitTest/Domain.UnitTest/RessourceTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Models;
+
+namespace UnitTest.Domain.UnitTest
+{
+    public class RessourceTests
+    {
+        [Fact]
+        public static void RessourceValidation_ShouldPass_WhenGivenCorrectInfo()
+        {
+            // Arrange
+            int ressourceId = 1;
+            string ressourceName = "Luksushytte nr. 5";
+            string ressourceType = "Hytte";
+            double ressourceBasePrice = 899.95;
+
+            // Act
+            Ressource ressource = new Ressource(ressourceId, ressourceName, ressourceType, ressourceBasePrice);
+
+            // Assert
+            Assert.Equal(ressourceId, ressource.ResourceID);
+            Assert.Equal(ressourceName, ressource.ResourceName);
+            Assert.Equal(ressourceType, ressource.ResourceType);
+            Assert.Equal(ressourceBasePrice, ressource.ResourceBasePrice);
+        }
+    }
+}
