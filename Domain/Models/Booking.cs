e678361944951e90abfd0c7f0b37714a789a1abf
Mikkel Schmidt Nielsen <mikkelschmidt@msn.com>
2025-11-13 11:26:38 +0100

    Created booking page and entity booking

---
+++ Domain/Models/Booking.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Booking
+    {
+        public int Id { get; private set; }
+        public DateOnly StartDate { get; private set; }
+        public DateOnly EndDate { get; private set; }
+        public decimal TotalPrice { get; private set; }
+
+        public Booking(DateOnly startDate, DateOnly endDate, decimal totalPrice)
+        {
+            StartDate = startDate;
+            EndDate = endDate;
+            TotalPrice = totalPrice;
+
+            ValidateBookingInformation();
+        }
+
+        private void ValidateBookingInformation()
+        {
+            // Start dato er i nutid
+            if (StartDate < DateOnly.FromDateTime(DateTime.Now))
+            {
+                throw new ArgumentException("Start datoen kan ikke være i fortiden");
+            }
+
+            // Start dato er sat før slut dato
+            if (StartDate > EndDate)
+            {
+                throw new ArgumentException("Slut datoen kan ikke være før start datoen");
+            }
+
+            // Start og slut dato er forskellige
+            if (StartDate == EndDate)
+            {
+                throw new ArgumentException("Start dato og slut dato må ikke være på samme dag");
+            }
+
+            // Total pris er positiv
+            if (TotalPrice < 0)
+            {
+                throw new ArgumentException("Den totale pris kan ikke være negativ");
+            }
+        }
+    }
+}
---
+++ UnitTest/Domain.UnitTest/BookingEntityTest.cs
@@ -0,0 +1,81 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.Domain.UnitTest
+{
+    public class BookingEntityTest
+    {
+        [Fact]
+        public void BookingCreation_ShouldPass_WhenGivenCorrectInformation()
+        {
+            // Arrange
+            DateOnly startDate = DateOnly.FromDateTime(DateTime.Now);
+            DateOnly endDate = DateOnly.FromDateTime(DateTime.Now.AddDays(2));
+            decimal totalPrice = 5000;
+
+            // Act
+            Booking booking = new Booking(startDate, endDate, totalPrice);
+
+            // Assert
+            Assert.Equal(startDate, booking.StartDate);
+            Assert.Equal(endDate, booking.EndDate);
+            Assert.Equal(totalPrice, booking.TotalPrice);
+        }
+
+        [Fact]
+        public void BookingCreation_ShouldFail_WhenGivenStartDateTimeInThePast()
+        {
+            // Arrange
+            DateOnly startDate = DateOnly.FromDateTime(DateTime.Now.AddDays(-1));
+            DateOnly endDate = DateOnly.FromDateTime(DateTime.Now.AddDays(2));
+            decimal totalPrice = 5000;
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(
+                () => new Booking(startDate, endDate, totalPrice));
+        }
+
+        [Fact]
+        public void BookingCreation_ShouldFail_WhenGivenStartDateIsEqualToEndDate()
+        {
+            // Arrange
+            DateOnly startDate = DateOnly.FromDateTime(DateTime.Now);
+            DateOnly endDate = DateOnly.FromDateTime(DateTime.Now);
+            decimal totalPrice = 5000;
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(
+                () => new Booking(startDate, endDate, totalPrice));
+        }
+
+        [Fact]
+        public void BookingCreation_ShouldFail_WhenGivenStartDateIsBeforeEndDate()
+        {
+            // Arrange
+            DateOnly startDate = DateOnly.FromDateTime(DateTime.Now);
+            DateOnly endDate = DateOnly.FromDateTime(DateTime.Now.AddDays(-1));
+            decimal totalPrice = 5000;
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(
+                () => new Booking(startDate, endDate, totalPrice));
+        }
+
+        [Fact]
+        public void BookingCreation_ShouldFail_WhenGivenTotalPriceIsLessThanZero()
+        {
+            // Arrange
+            DateOnly startDate = DateOnly.FromDateTime(DateTime.Now);
+            DateOnly endDate = DateOnly.FromDateTime(DateTime.Now.AddDays(2));
+            decimal totalPrice = -1;
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(
+                () => new Booking(startDate, endDate, totalPrice));
+        }
+    }
+}
