afeefb6581212acccdccdbf14dc883a7de940132
Mikkel Schmidt Nielsen <mikkelschmidt@msn.com>
2025-11-14 07:53:08 +0100

    Made configuration file for booking

---
+++ Persistence/EntityFramework/EfModelConfigurations/BookingConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.EntityFramework.EfModelConfigurations
+{
+    internal class BookingConfiguration : IEntityTypeConfiguration<Booking>
+    {
+        public void Configure(EntityTypeBuilder<Booking> builder)
+        {
+            builder.ToTable("Booking");
+            builder.HasKey(booking => booking.Id);
+
+            // Relations
+        }
+    }
+}
