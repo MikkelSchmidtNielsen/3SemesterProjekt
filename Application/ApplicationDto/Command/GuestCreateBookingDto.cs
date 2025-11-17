eae9401c0f58078bc96799cee5d0d35a832570d6
AskGamKru <kruseaske@gmail.com>
2025-11-17 10:52:00 +0100

    Created DTO templates: GuestCreateBookingDto, GuestCreateUserDto & GetResourceDto

---
+++ Application/ApplicationDto/Command/GuestCreateBookingDto.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.ApplicationDto.Command
+{
+    public class GuestCreateBookingDto
+    {
+        public Guest GuestId { get; set; }
+        public Resource ResourceId { get; set; }
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
---
+++ Application/ApplicationDto/Query/GetResourceDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.ApplicationDto.Query
+{
+    public class GetResourceDto
+    {
+        public int Id { get; set; }
+    }
+}
