c257cb677e34c98785ef0d7bb9c70b5f501a51f6
AskGamKru <kruseaske@gmail.com>
2025-11-17 10:52:57 +0100

    Added folder: ApplicationDto and subfolders: Command & Query

--- Application/ApplicationDto/Command/GuestCreateBookingDto.cs
+++ Application/ApplicationDto/Command/GuestCreateBookingDto.cs
@@ -9,8 +9,8 @@ namespace Application.ApplicationDto.Command
 {
     public class GuestCreateBookingDto
     {
-        public Guest GuestId { get; set; }
-        public Resource ResourceId { get; set; }
+        public int GuestId { get; set; }
+        public int ResourceId { get; set; }
         public DateOnly StartDate { get; set; }
         public DateOnly EndDate { get; set; }
         public decimal TotalPrice { get; set; }
