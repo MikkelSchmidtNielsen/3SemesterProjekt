3dedd903b4a92af70f42ea6c764006de8ed976de
Sarah <peasblinky@gmail.com>
2025-11-18 10:05:28 +0100

    Update

--- Persistence/EntityFramework/SqlServerDbContext.cs
+++ Persistence/EntityFramework/SqlServerDbContext.cs
@@ -6,12 +6,11 @@ namespace Persistence.EntityFramework
 {
     public class SqlServerDbContext : DbContext
     {
-        public DbSet<Booking> Bookings { get; set; } 
         public DbSet<Resource> Resources { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=Camping;Integrated Security=SSPI;TrustServerCertificate=true");
+            optionsBuilder.UseSqlServer("Server=localhost;Database=Camping;Integrated Security=SSPI;Encrypt=False");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
