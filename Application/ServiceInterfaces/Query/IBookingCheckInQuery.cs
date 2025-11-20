14173e66b9e17fbf0bf72caaf964ddf2fda782a5
Sarah <peasblinky@gmail.com>
2025-11-20 13:02:45 +0100

    Created IBookingCheckInQuery.cs and BookingCheckInQuery.cs

---
+++ Application/ServiceInterfaces/Query/IBookingCheckInQuery.cs
@@ -0,0 +1,16 @@
+﻿using Common.ResultInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Models;
+
+namespace Application.ServiceInterfaces.Query
+{
+    public interface IBookingCheckInQuery
+    {
+        Task<IResult<IEnumerable<Booking>>> GetActiveBookingsWithMissingCheckIns();
+
+    }
+}
---
+++ Application/Services/Query/BookingCheckInQuery.cs
@@ -0,0 +1,27 @@
+﻿using Application.RepositoryInterfaces;
+using Application.ServiceInterfaces.Query;
+using Common.ResultInterfaces;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services.Query
+{
+    public class BookingCheckInQuery : IBookingCheckInQuery
+    {
+        private readonly IBookingRepository _repository;
+
+        public BookingCheckInQuery(IBookingRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<IResult<IEnumerable<Booking>>> GetActiveBookingsWithMissingCheckIns()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
