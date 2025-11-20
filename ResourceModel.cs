75b275d62630fc92e9aa38953ddff89e757e88fa
Sarah <peasblinky@gmail.com>
2025-11-20 09:46:39 +0100

    Updated ResourceFactoryTest.cs

--- ResourceModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-public class ResourceModel
-{
-    public string Name { get; set; }
-    public string Type { get; set; }
-    public decimal BasePrice { get; set; }
-    public string? Description { get; set; }
-}
--- UnitTest/Application.UnitTest/CreateResourceServiceTest.cs
+++ UnitTest/Application.UnitTest/CreateResourceServiceTest.cs
@@ -1,20 +1,22 @@
-﻿using Application.RepositoryInterfaces;
+﻿using Application.Factories;
+using Application.RepositoryInterfaces;
 using Application.Services.Command;
 using Common;
+using Domain.Models;
+using Domain.ModelsDto;
 using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Domain.ModelsDto;
 
 namespace UnitTest.Application.UnitTest
 {
     public class CreateResourceServiceTest
     {
         [Fact]
-        public static void ResourceCreation_ShouldPass_WhenGivenCorrectInfo()
+        public async Task ResourceCreation_ShouldPass_WhenGivenCorrectInfo()
         {
             // Arrange
             var dto = new CreateResourceDto
@@ -24,7 +26,18 @@ namespace UnitTest.Application.UnitTest
                 BasePrice = 599.95M,
                 Description = "Seniorvenlig hytte"
             };
-            
+
+            Mock<IResourceRepository> repository = new Mock<IResourceRepository>();
+            repository.Setup(x => x.GetResourceByResourceNameAsync(dto.Name)).ReturnsAsync(Result<Resource>.Error(null, new Exception("En ressource med dette navn eksisterer ikke.")));
+            repository.Setup(x => x.GetResourceByLocationAsync(dto.Location)).ReturnsAsync(Result<Resource>.Error(null, new Exception("Der findes allerede en ressource på denne placering.")));
+
+            ResourceFactory resourceFactory = new ResourceFactory(repository.Object);
+
+            // Act
+            var result = await resourceFactory.CreateResourceAsync(dto);
+
+            // Assert
+            Assert.True(result.IsSucces());
 
         }
     }
