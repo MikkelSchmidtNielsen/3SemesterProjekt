be0db951358ff58b2b8eca8455aa280ef807a21e
MikkelRLarsen <Thenuker95@gmail.com>
2025-11-20 10:38:43 +0100

    Added Initial Api

---
+++ Microservices/Api/Controllers/Login.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class Login : ControllerBase
+	{
+		[HttpPost]
+		public IActionResult RegisterUser([FromBody] string email)
+		{
+
+		}
+	}
+}
