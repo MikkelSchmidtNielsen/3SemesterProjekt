f2f6688be58ee4a85d7a001ac67b0ad35a2a2640
Sarah <peasblinky@gmail.com>
2025-11-24 08:31:41 +0100

    Seperated C# code from Razor page

---
+++ Presentation/Presentation.Server/Components/Pages/AdminPages/CampingAdministration.razor.cs
@@ -0,0 +1,23 @@
+﻿using Application.ApplicationDto.Query;
+
+namespace Presentation.Server.Components.Pages.AdminPages
+{
+    public partial class CampingAdministration
+    {
+        DateOnly currentDate = DateOnly.FromDateTime(DateTime.Now);
+        bool checkInActive = true;
+        bool checkOutActive = false;
+
+        IEnumerable<BookingMissingCheckInQueryDto> _missingCheckIns = new List<BookingMissingCheckInQueryDto>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            var resultOfMissingCheckIns = await _checkInQuery.GetActiveBookingsWithMissingCheckInsAsync();
+
+            if (resultOfMissingCheckIns.IsSucces())
+            {
+                _missingCheckIns = resultOfMissingCheckIns.GetSuccess().OriginalType;
+            }
+        }
+    }
+}
