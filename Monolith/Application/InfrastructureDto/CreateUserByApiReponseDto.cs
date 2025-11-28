85ed6a5ff3544efee8be16fd5621787b885b219c
MikkelRLarsen <Thenuker95@gmail.com>
2025-11-28 10:20:12 +0100

    Moved changed ApplicationLayer

---
+++ Monolith/Application/InfrastructureDto/CreateUserByApiReponseDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.InfrastructureDto
+{
+	public class CreateUserByApiReponseDto
+	{
+		/// <summary>
+		/// JSON Web Token (JWT) til brug ved login/autorisation
+		/// </summary>
+		public string JwtToken { get; set; }
+	}
+}
--- Monolith/InversionOfControlContainers/InversionOfControl/HttpClientSetup/HttpClientModule.cs
+++ Monolith/InversionOfControlContainers/InversionOfControl/HttpClientSetup/HttpClientModule.cs
@@ -1,10 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Infrastructure.InternalApiCalls.UserAuthenticationApi;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Refit;
 
 namespace InversionOfControlContainers.InversionOfControl.HttpClientSetup
 {
@@ -15,8 +12,27 @@ namespace InversionOfControlContainers.InversionOfControl.HttpClientSetup
             // Creates Microsofts IHttpClientFactory
             services.AddHttpClient();
 
-            // Register named clients
+			// Register named clients
+			services.AddHttpClient("Authentication", client =>
+			{
+				client.BaseAddress = new Uri(configuration["ApiBaseUrls:Authentication"]!);
+				client.Timeout = TimeSpan.FromSeconds(30);
+			});
 
-        }
+			RegisterRefit(services);
+		}
+
+		private static void RegisterRefit(IServiceCollection services)
+		{
+			// Adds Refit Interface by getting it from IHttpClientFactory at runtime
+			services
+				.AddRefitClient<IUserAuthenticationApi>()
+				.ConfigureHttpClient((sp, client) =>
+				{
+					var factory = sp.GetRequiredService<IHttpClientFactory>();
+					var http = factory.CreateClient("Authentication");
+					client.BaseAddress = http.BaseAddress;
+				});
+		}
     }
 }
