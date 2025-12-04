7355610c63beeb0ca6e7246ba5fefc78537bc035
Sarah <peasblinky@gmail.com>
2025-12-04 11:37:03 +0100

    Update

--- Microservices/Authentication/Application/Services/Command/CreateOptCommandHandler.cs
+++ Microservices/Authentication/Application/Services/Command/CreateOptCommandHandler.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 
 namespace Application.Services.Command
 {
@@ -16,7 +18,6 @@ namespace Application.Services.Command
             int value = random.Next(100000, 999999);
             DateTime expiryTime = DateTime.UtcNow.AddMinutes(30);
             Otp otp = new Otp(value, expiryTime);
-
 
         }
     }
