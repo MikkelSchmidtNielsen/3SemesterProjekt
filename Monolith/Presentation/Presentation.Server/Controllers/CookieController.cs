5c6e562a34d8604b2da3215986ccc4ca01032de3
Sarah <peasblinky@gmail.com>
2025-12-09 19:18:49 +0100

    Updated CookieController.cs

--- Monolith/Presentation/Presentation.Server/Controllers/CookieController.cs
+++ Monolith/Presentation/Presentation.Server/Controllers/CookieController.cs
@@ -4,12 +4,16 @@ namespace Presentation.Server.Controllers
 {
     public class CookieController : Controller
     {
-        public void SetCookie(string key, string token)
+        public void SetCookie(string token)
         {
-            CookieOptions options = new CookieOptions();
-            DateTime expiryTime = DateTime.UtcNow.AddHours(4);
+            CookieOptions options = new CookieOptions
+            {
+                HttpOnly = true, // Ensures that JavaScript code can't access this cookie
+                Secure = true,
+                Expires = DateTime.UtcNow.AddDays(1)
+            };
 
-            Response.Cookies.Append(key, token, expiryTime);
+            Response.Cookies.Append("auth_cookie", token, options);
         }
     }
 }
