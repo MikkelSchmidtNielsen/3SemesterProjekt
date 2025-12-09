0fb8d19b593030affa2774dc5a798477eaab318b
Sarah <peasblinky@gmail.com>
2025-12-09 15:14:15 +0100

    Miniupdate

---
+++ Monolith/Presentation/Presentation.Server/Controllers/CookieController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Server.Controllers
+{
+    public class CookieController : Controller
+    {
+        public void SetCookie(string key, string token)
+        {
+            CookieOptions options = new CookieOptions();
+            DateTime expiryTime = DateTime.UtcNow.AddHours(4);
+
+            Response.Cookies.Append(key, token, expiryTime);
+        }
+    }
+}
