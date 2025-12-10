afb9da5663c444c396d6f2a4437b561d717b699f
MikkelRLarsen <Thenuker95@gmail.com>
2025-12-10 15:13:26 +0100

    TestCookieMiddleware

--- Monolith/Presentation/Presentation.Server/JwtCookieMiddleware.cs
+++ Monolith/Presentation/Presentation.Server/JwtCookieMiddleware.cs
@@ -10,6 +10,7 @@ namespace Presentation.Server
 		{
 			if (context.Request.Cookies.TryGetValue("authCookie", out var token))
 			{
+				//var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxIiwiZW1haWwiOiJtaWtrZWxAcm9zZW5kYWhsbGFyc2VuLmRrIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiR3Vlc3QiLCJleHAiOjE3NjUzNzc3NjZ9.XWPlva_6fzUhMs-1GrscCaysLUGB4zX2qK5AHp-16Uc";
 				try
 				{
 					var handler = new JwtSecurityTokenHandler();
