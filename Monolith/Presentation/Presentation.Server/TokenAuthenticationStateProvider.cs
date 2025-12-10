3e66f0e0b0c236ff7dec794f5b5847d2773ab0d5
Sarah <peasblinky@gmail.com>
2025-12-10 07:49:43 +0100

    Added TokenAuthenticationProvider.cs

---
+++ Monolith/Presentation/Presentation.Server/TokenAuthenticationStateProvider.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+
+namespace Presentation.Server
+{
+    /* This custom AuthenticationStateProvider is necessary in order to use AuthorizeView in Blazor. */
+    public class TokenAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        /* IHttpContextAccessorr is used to access data from the Httponly Cookie. */
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public TokenAuthenticationStateProvider(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
