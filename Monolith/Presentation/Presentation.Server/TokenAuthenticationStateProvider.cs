40643f7932dd173489aa274f88981e9238fc62fd
Sarah <peasblinky@gmail.com>
2025-12-10 07:51:48 +0100

    Updated comments

--- Monolith/Presentation/Presentation.Server/TokenAuthenticationStateProvider.cs
+++ Monolith/Presentation/Presentation.Server/TokenAuthenticationStateProvider.cs
@@ -5,7 +5,7 @@ namespace Presentation.Server
     /* This custom AuthenticationStateProvider is necessary in order to use AuthorizeView in Blazor. */
     public class TokenAuthenticationStateProvider : AuthenticationStateProvider
     {
-        /* IHttpContextAccessorr is used to access data from the Httponly Cookie. */
+        /* IHttpContextAccessorr is used to access data from an HttpOnly Cookie. In this case, it's authCookie from CookieController.cs */
         private readonly IHttpContextAccessor _httpContextAccessor;
 
         public TokenAuthenticationStateProvider(IHttpContextAccessor httpContextAccessor)
@@ -14,7 +14,7 @@ namespace Presentation.Server
         }
         public override Task<AuthenticationState> GetAuthenticationStateAsync()
         {
-            throw new NotImplementedException();
+            var cookie = _httpContextAccessor.HttpContext.Request.Cookies["authCookie"];
         }
     }
 }
